08872bb1dbc5ac7beffd7826dadd74611f81e82a
Andy Pickering <andy.nihon@gmail.com>
2011-07-21 22:46:47 +0900

    initial commit

---
+++ game.rb
@@ -0,0 +1,79 @@
+require_relative "strategy"
+
+module Sudoku
+  # Represents a game of sudoku
+  class Game
+    attr_reader :grid
+
+    def initialize(grid, strategy = nil)
+      @grid = grid.map {|row|
+        row.map {|cell|
+          raise "Invalid value '#{cell}' (expected integer from 1 to 9 or nil)" unless (1..9) === cell || cell.nil?
+          cell.nil? ? (1..9).to_a : [cell]
+        }
+      }
+      @strategy = strategy || Strategy::MostConstrainedCellStrategy.new
+      @strategy.reduce_all!(@grid)
+    end
+
+    def [](x, y)
+      @grid[x][y]
+    end
+
+    def []=(x, y, val)
+      @grid[x][y] = [val]
+      @strategy.reduce!(@grid, x, y, val)
+    end
+
+    def clone
+      Marshal.load(Marshal.dump(self))
+    end
+
+    def to_s
+      @grid.map {|row|
+        row.map {|cell| cell.length > 1 ? "."
+                                        : cell.empty? ? "x" : cell.first}.join
+      }.join("\n")
+    end
+
+    def solved?
+      # Solved if every cell has one single possible value remaining
+      !@grid.detect {|row| row.detect {|cell| cell.length != 1}}
+    end
+
+    def invalid?
+      # Invalid if any cell is empty (no remaining possible values)
+      !!@grid.detect {|row| row.detect {|cell| cell.empty?}}
+    end
+
+    # Returns a solution to the puzzle if one exists or returns nil if no solution exists
+    def solution
+      answer = self.clone.solve
+      answer.nil? ? nil : answer.grid.map {|row| row.map {|cell| cell.first}}
+    end
+
+    # Returns a copy of this Game with all cells assigned values that represent a solution to the puzzle
+    # If there are multiple solutions, the first one found is returned
+    # If no solution exists, nil is returned
+    def solve
+      if invalid?
+        nil
+      elsif solved?
+        self
+      else
+        x, y = @strategy.next_cell(@grid)
+
+        # Try each of the possible values for cell [x, y] and if one leads to a solution, return that solution
+        self[x, y].each {|val|
+          next_game = self.clone
+          next_game[x, y] = val
+          next_solution = next_game.solve
+          return next_solution unless next_solution.nil?
+        }
+
+        # No solution found
+        nil
+      end
+    end
+  end
+end
---
+++ strategy.rb
@@ -0,0 +1,90 @@
+module Sudoku
+  module Strategy
+    # Represents a strategy for solving a sudoku puzzle
+    class Strategy
+      # Reduce (simplify) the sudoku problem based on the knowledge that cell (x, y) was just set to val
+      def reduce!(grid, x, y, val)
+        raise "This method should be overridden"
+      end
+
+      # Decide which cell should next have it's value guessed by the solver
+      # Should "return x, y" to return the row and column indices of the cell
+      def next_cell(grid)
+        raise "This method should be overridden"
+      end
+
+      # Return a cell's index "group" (0, 1, 2), (3, 4, 5) or (6, 7, 8)
+      def group(i)
+        case i
+        when 0..2 then (0..2).to_a
+        when 3..5 then (3..5).to_a
+        when 6..8 then (6..8).to_a
+        end
+      end
+    end
+
+
+    # Very basic strategy for solving sudoku
+    # Just scans through the unsolved cells from top-left to bottom-right and sets the first one if finds to the lowest
+    # raining possible value for that cell
+    class FirstCellStrategy < Strategy
+      def reduce_one!(grid, x, y, val)
+        (0..8).each {|i|
+          grid[i][y].delete(val) unless i == x
+          grid[x][i].delete(val) unless i == y
+        }
+        group(x).each {|i|
+          group(y).each {|j|
+            grid[i][j].delete(val) unless i == x && j == y
+          }
+        }
+      end
+
+      def reduce_all!(grid)
+        start_grid = grid.clone
+        (0..8).each {|x|
+          (0..8).each {|y|
+            reduce_one!(grid, x, y, grid[x][y].first) if grid[x][y].length == 1
+          }
+        }
+
+        # Reduce again if any change was made
+        reduce_all!(grid) unless start_grid == grid
+      end
+
+      # Just removes possible values from other cells in the same row, col and group
+      def reduce!(grid, x, y, val)
+        reduce_one!(grid, x, y, val)
+        reduce_all!(grid)
+      end
+
+      # Returns the first unsolved cell we find (no attempt to use heuristics)
+      def next_cell(grid)
+        grid.each_with_index {|row, x|
+          row.each_with_index {|cell, y|
+            return x, y if cell.length > 1
+          }
+        }
+      end
+    end
+
+
+    # Very simple heuristic based solver
+    # Always solves the "most constrained" cell (the cell with the fewest remaining possible values)
+    class MostConstrainedCellStrategy < FirstCellStrategy
+      def next_cell(grid)
+        min_possibilities, min_x, min_y = nil, nil, nil
+        grid.each_with_index {|row, x|
+          row.each_with_index {|cell, y|
+            if cell.length > 1 && (min_possibilities.nil? || cell.length < min_possibilities)
+              min_possibilities = cell.length
+              min_x = x
+              min_y = y
+            end
+          }
+        }
+        return min_x, min_y
+      end
+    end
+  end
+end
